69af1f77b4ce1465337f36c85850793ad9170b5b
Shubham <shubhamsachdeva593@gmail.com>
2020-09-13 00:52:04 +0530

    deploy

--- js/scripts.js
+++ js/scripts.js
@@ -61,8 +61,7 @@ form.addEventListener("click", (e) => {
   var code = xe.getValue();
   var language = document.getElementById("language").value;
   var input = document.getElementById("input").value;
-  const url =
-    "http://ec2-15-207-249-131.ap-south-1.compute.amazonaws.com/compile";
+  const url = "ec2-15-207-249-131.ap-south-1.compute.amazonaws.com/compile";
   return fetch(url, {
     method: "POST",
     body: JSON.stringify({
